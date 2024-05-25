58ab26589885f9265cde2e41a070ec30635b09ba
Brett Orton <brettorton1@gmail.com>
2024-05-25 16:20:16 -0600

    Add arrays of options for prompt variables

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-const getRandomNumber = (num) = Math.floor(Math.random()* num);
+const getRandomNumber = (num) => Math.floor(Math.random()* num);
+
+const mainCharacterOptions = ['hardboiled detective', 'princess living in exile', 'stranger with no memories', 'youth with unique abilities', 'single mother', 'scarred veteran', 'man with absolutely nothing special about him'];
+
+const settingOptions = ['a fantasy world rife with magic', 'a civilization spanning the galaxy, with technologies we can scarcely imagine', 'an isolated midwestern town with a dark secret', 'a bustling metropolis', 'a lawless frontier'];
+
+const scaleOptions = ['child', 'home', 'nation', 'world'];
+
+const threatOptions = ['a deadly virus', 'an ancient enemy', 'creatures out of a nightmare', 'a natural disaster of cataclysmic proportions', 'invaders from another world', 'a deranged killer with strange abilities', 'a mysterious organization with seemingly unlimited resources']; 
 
